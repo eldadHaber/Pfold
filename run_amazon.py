c4b8849f66efd008a035d3b7bf84b569a05a11f8
Tue Boesen <tue.boesen@gmail.com>
2020-11-11 20:11:06 -0800

    Run trRosetta network on amazon

--- run_amazon.py
+++ run_amazon.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     parser.add_argument('--basefolder', default=os.path.basename(__file__).split(".")[0], type=str, metavar='N', help='Basefolder where results are saved')
     parser.add_argument('--mode', default='standard', type=str, metavar='N', help='Mode to run in (debug,fast,paper)')
     # data
-    parser.add_argument('--dataset-train', default='./../data/train/', type=str, metavar='N', help='Name of dataset to run, currently implemented: ')
-    parser.add_argument('--dataset-test', default='./../data/test/', type=str, metavar='N', help='Name of dataset to run, currently implemented: ')
+    parser.add_argument('--dataset-train', default='./../data/final_dataset_2d_all_train/', type=str, metavar='N', help='Name of dataset to run, currently implemented: ')
+    parser.add_argument('--dataset-test', default='./../data/final_dataset_2d_all_validate/', type=str, metavar='N', help='Name of dataset to run, currently implemented: ')
     # parser.add_argument('--dataset-train', default='D:/Dropbox/ComputationalGenetics/data/final_dataset_1d/', type=str, metavar='N', help='Name of dataset to run, currently implemented: ')
     # parser.add_argument('--dataset-test', default='D:/Dropbox/ComputationalGenetics/data/final_dataset_1d/', type=str, metavar='N', help='Name of dataset to run, currently implemented: ')
 
@@ -20,17 +20,17 @@ if __name__ == '__main__':
     parser.add_argument('--seq-flip-prop', default=0.5, type=float, metavar='N', help='Input feature types')
     parser.add_argument('--feature-dim', default=1, type=int, metavar='N', help='Input feature types')
     parser.add_argument('--inpainting', default=False, type=bool, metavar='N', help='Input feature types')
-    parser.add_argument('--i-seq', default=False, type=bool, metavar='N', help='Input feature types')
+    parser.add_argument('--i-seq', default=True, type=bool, metavar='N', help='Input feature types')
     parser.add_argument('--i-pssm', default=True, type=bool, metavar='N', help='Input feature types')
     parser.add_argument('--i-entropy', default=True, type=bool, metavar='N', help='Input feature types')
-    parser.add_argument('--i-cov_all', default=False, type=bool, metavar='N', help='Input feature types')
-    parser.add_argument('--i-cov', default=True, type=bool, metavar='N', help='Input feature types')
+    parser.add_argument('--i-cov_all', default=True, type=bool, metavar='N', help='Input feature types')
+    parser.add_argument('--i-cov', default=False, type=bool, metavar='N', help='Input feature types')
     parser.add_argument('--i-contact', default=True, type=bool, metavar='N', help='Input feature types')
     parser.add_argument('--random-crop', default=False, type=bool, metavar='N', help='Input feature types')
 
     # Learning
-    parser.add_argument('--network', default='vnet', type=str, metavar='N', help='network to use')
-    parser.add_argument('--batch-size', default=20, type=int, metavar='N', help='batch size used in dataloader')
+    parser.add_argument('--network', default='dilated_resnet', type=str, metavar='N', help='network to use')
+    parser.add_argument('--batch-size', default=1, type=int, metavar='N', help='batch size used in dataloader')
     parser.add_argument('--SL-lr', default=1e-3, type=float, metavar='N', help='Learning Rate')
     parser.add_argument('--max-iter', default=200000, type=int, metavar='N', help='select the neural network to train (resnet)')
     parser.add_argument('--report-iter', default=1000, type=int, metavar='N', help='select the neural network to train (resnet)')
